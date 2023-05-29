b1f8f5c9d9e9c3e9f4f3b339d90fc960e839f580
juanjohell <juanchoped@gmail.com>
2023-05-29 18:25:22 +0200

    Inserción de modelo en base de datos

--- modelTracker/conexion_bbdd.py
+++ modelTracker/conexion_bbdd.py
@@ -23,9 +23,34 @@ def create_connection():
     return connection
 
 def crea_base_de_datos():
-    connection = create_connection(path)
+    connection = create_connection()
     # Crear las tablas de la base de datos
     cursor_obj = connection.cursor()
+    print('creación de modelo')
     cursor_obj.execute(crear_tabla_usos)
-    cursor_obj.execute(crear_tabla_optimizer)
+    cursor_obj.execute(crear_tabla_optimizador)
+    cursor_obj.execute(crear_tabla_familia_modelo)
     cursor_obj.execute(crear_tabla_modelos)
+    connection.commit()
+
+    #INSERCION DE DATOS MAESTROS:
+    # Tabla de usos:
+    print('insercion de datos')
+    fila = ("Clasificación","Modelo diseñado para clasificación de imágenes")
+    insert_tabla_usos(connection,fila)
+    fila = ("Detección","Modelo diseñado para detección de objetos en imágenes")
+    insert_tabla_usos(connection,fila)
+
+    # Tabla de optimizadores
+    fila = ("Adam",0.001,1,0.0,0.9,0.999,0.00000001)
+    insert_tabla_optimizador(connection,fila)
+
+    # Tabla familia_modelo
+    fila = ("VGG19","Modelo vgg19", 26, "(224,224,3)")
+    insert_tabla_familia_modelo(connection,fila)
+
+    # Tabla Modelos, se inserta el modelo de ejemplo de la aplicación
+    fila = (1,1, "vgg19_imagenet.h4", 1, "Modelo VGG9 de Keras entrenado en Imagenet", 26, "(224,224,3)")
+    insert_tabla_modelos(connection,fila)
+
+crea_base_de_datos()
--- modelTracker/sql/modelo_bbdd.py
+++ modelTracker/sql/modelo_bbdd.py
@@ -8,29 +8,28 @@ Created on Thu May 11 13:32:34 2023
 crear_tabla_modelos = '''CREATE TABLE "Modelos" (
     "id_modelo" INTEGER NOT NULL UNIQUE,
     "id_uso" INTEGER NOT NULL,
-    "id_optimizer"	INTEGER NOT NULL,
+    "id_optimizer"	INTEGER,
 	"nombre"	TEXT NOT NULL,
 	"id_familia"	TEXT NOT NULL,
     "descripcion"	TEXT,
 	"depth"	INTEGER,
 	"input_shape"	TEXT,
 	PRIMARY KEY("id_modelo"),
-	FOREIGN KEY("id_optimizer") REFERENCES "Optimizer"("id_optimizador"),
+	FOREIGN KEY("id_optimizer") REFERENCES "Optimizador"("id_optimizador"),
 	FOREIGN KEY("id_familia") REFERENCES "FamiliaModelo"("id_familia"),
     FOREIGN KEY("id_uso") REFERENCES "Usos"("id_uso")
 );'''
 
 crear_tabla_optimizador = '''CREATE TABLE "Optimizador" (
-	"id_optimizador"	INTEGER NOT NULL UNIQUE,
-    "optimizer_type"	TEXT NOT NULL,
-	"learning_rate"	REAL NOT NULL,
-	"amsgrad"	INTEGER NOT NULL,
+	"id_optimizador" INTEGER NOT NULL UNIQUE,
+    "optimizer_type" TEXT NOT NULL,
+	"learning_rate"	REAL,
+	"amsgrad" INTEGER,
 	"decay"	REAL,
 	"b1"	REAL,
 	"b2"	REAL,
 	"epsilon"	REAL,
-	PRIMARY KEY("id_optimizer"),
-	UNIQUE("learning_rate","amsgrad","decay","b1","b2","epsilon")
+	PRIMARY KEY("id_optimizador")
 );'''
 
 crear_tabla_usos = '''CREATE TABLE "Usos" (
@@ -56,7 +55,6 @@ def insert_tabla_usos(conn, params):
     VALUES (?,?)'''
     
     cursor_obj = conn.cursor()
-
     cursor_obj.execute(sql,params)
     conn.commit()
 
@@ -64,8 +62,8 @@ def insert_tabla_usos(conn, params):
     
 # REALIZA INERCIÓN EN TABLA DE OPTIMIZADORES
 
-def insert_tabla_optimizers(conn, params):
-    sql = '''INSERT OR IGNORE INTO OPTIMIZER (OPTIMIZER_TYPE, LEARNING_RATE, AMSGRAD,
+def insert_tabla_optimizador(conn, params):
+    sql = '''INSERT OR IGNORE INTO OPTIMIZADOR (OPTIMIZER_TYPE, LEARNING_RATE, AMSGRAD,
     DECAY, B1, B2, EPSILON) VALUES (?,?,?,?,?,?,?)'''
     
     cursor_obj = conn.cursor()
@@ -79,11 +77,19 @@ def insert_tabla_optimizers(conn, params):
 # RETORNA EL ID ASIGNADO AL REGISTRO.
 
 def insert_tabla_modelos(conn, params):
-    sql = '''INSERT OR IGNORE INTO MODELOS (ID_USO, ID_OPTIMIZER, NOMBRE, FAMILIA, 
+    sql = '''INSERT OR IGNORE INTO MODELOS (ID_USO, ID_OPTIMIZER, NOMBRE, ID_FAMILIA, 
     DESCRIPCION, DEPTH, INPUT_SHAPE) VALUES (?,?,?,?,?,?,?)'''
     
     cursor_obj = conn.cursor()
+    cursor_obj.execute(sql,params)
+    conn.commit()
 
+    return cursor_obj.lastrowid
+
+def insert_tabla_familia_modelo(conn, params):
+    sql = '''INSERT OR IGNORE INTO FAMILIAMODELO (NOMBRE, DESCRIPCION, DEPTH, INPUT_SHAPE) VALUES (?,?,?,?)'''
+
+    cursor_obj = conn.cursor()
     cursor_obj.execute(sql,params)
     conn.commit()
 
